2d59a7a3a17ed3de9b3be445c9083e22ecaacb45
anandu-github <124525582+anandu-github@users.noreply.github.com>
2024-02-16 21:52:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello():
-    return 'Hello, World! fask automation test'
+    return 'Hello, World! fask automation test 2'
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
