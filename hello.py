213de8c7fae97d1d8955494b311ae76be68fa5ad
anandu-github <124525582+anandu-github@users.noreply.github.com>
2024-02-16 22:52:23 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello():
-    return 'Hello, World! fask automation test 4'
+    return 'Flask app ci/cd pipe-line is ready!!'
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
