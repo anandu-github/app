f364dad6e65d3d3e29b9a9e866edcf61a2e69e40
anandu-github <124525582+anandu-github@users.noreply.github.com>
2024-02-18 16:48:39 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello():
-    return 'hello Flask app CI/CD pipe-line is ready!!'
+    return 'Flask app CI/CD pipe-line is ready!!'
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
